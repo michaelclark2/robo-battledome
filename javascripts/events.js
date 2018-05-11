83f7baf71cd5085e945415f723f4fe0105561b6f
michael <michael.clark1992@gmail.com>
2018-05-10 22:30:27 -0500

    added attack function, display winner in a modal

--- javascripts/events.js
+++ javascripts/events.js
@@ -17,14 +17,52 @@ const chooseFighters = (e) => {
 
     player1.name = robot1Name;
     player2.name = robot2Name;
+    player1.maxHP = player1.hp;
+    player2.maxHP = player2.hp;
 
     data.setPlayers(player1, player2);
 
     dom.printPlayers(data.getPlayers());
+    addAttackEvent();
 
     $('#start, #choose-fighters').hide();
   }
 };
+const addAttackEvent = () => {
+  $('#attack').click(attack);
+};
+const attack = (e) => {
+  const players = data.getPlayers();
+  players[1].hp -= players[0].attack();
+  players[0].hp -= players[1].attack();
+  $('#hpPlayer1').width(`${Math.ceil((players[0].hp / players[0].maxHP) * 100)}%`);
+  $('#hpPlayer2').width(`${Math.ceil((players[1].hp / players[1].maxHP) * 100)}%`);
+  if (players[0].hp <= 0 && players[1].hp <= 0) {
+    $('#killscreen').modal('show');
+    $('#killscreen .modal-title').text('Draw');
+    $('#killscreen .modal-body').html('<p>There were no survivors...</p>');
+    resetBtn();
+  }
+  else if (players[0].hp <= 0) {
+    $('#killscreen').modal('show');
+    $('#killscreen .modal-title').text(`${players[1].name} is the victor!`);
+    $('#killscreen .modal-body').html(`<p>${players[1].model} ${players[1].type} ${players[1].name} has defeated ${players[0].model} ${players[0].type} ${players[0].name} by ${players[1].dmgType}.</p>`);
+    resetBtn();
+  }
+  else if (players[1].hp <= 0) {
+    $('#killscreen').modal('show');
+    $('#killscreen .modal-title').text(`${players[0].name} is the victor!`);
+    $('#killscreen .modal-body').html(`<p>${players[0].model} ${players[0].type} ${players[0].name} has defeated ${players[1].model} ${players[1].type} ${players[1].name} by ${players[0].dmgType}.</p>`);
+    resetBtn();
+  }
+
+};
+const resetBtn = () => {
+  $('#reset').click(e => {
+    location.reload();
+  });
+};
+
 module.exports = {
   addStartEvent,
 };
