e086d1fd5e2ccf8c4e3461e331428eb66e96254e
michael <michael.clark1992@gmail.com>
2018-05-27 09:13:38 -0500

    changed img size

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -43,7 +43,7 @@ const printOpponent = (opponent) => {
         <div id='opponentHP' class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
         </div>
       </div>
-      <img src="https://robohash.org/${opponent.model}${opponent.type}${opponent.hp}.png?size=500x500">
+      <img src="https://robohash.org/${opponent.model}${opponent.type}${opponent.hp}.png?size=450x450">
     </div>
   </div>
 </div>`;
